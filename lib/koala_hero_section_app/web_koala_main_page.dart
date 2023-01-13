d2e390ac7ec0dd144968c3e6a55b1a7b56af6563
Khine Lin <56064504+KhineKhineLin@users.noreply.github.com>
2023-01-13 23:30:00 +0630

    add text

--- lib/koala_hero_section_app/web_koala_main_page.dart
+++ lib/koala_hero_section_app/web_koala_main_page.dart
@@ -1,4 +1,3 @@
-import 'package:dribble_ui_flutter_5/user_profile_app/main_page.dart';
 import 'package:flutter/material.dart';
 
 class KoalaWebApp extends StatelessWidget {
@@ -37,6 +36,57 @@ class _MainPageState extends State<MainPage> {
                       flex: 1,
                       child: Container(
                         decoration: const BoxDecoration(color: Colors.brown),
+                        child: Column(
+                          children: [
+                            Expanded(
+                                flex: 2,
+                                child: Container(
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.center,
+                                    children: const [
+                                      Icon(
+                                        Icons.face,
+                                        color: Colors.white,
+                                        size: 38,
+                                      ),
+                                      Text(
+                                        'Koala',
+                                        style: TextStyle(
+                                            fontSize: 48,
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.bold),
+                                      )
+                                    ],
+                                  ),
+                                )),
+                            Expanded(
+                                flex: 5,
+                                child: Container(
+                                  padding: const EdgeInsets.only(left: 84),
+                                  child: Column(
+                                    children: [
+                                      Text(
+                                        'Get into Koala\'s beautiful world!',
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .headline2!
+                                            .copyWith(
+                                                fontWeight: FontWeight.bold,
+                                                fontSize: 72,
+                                                color: Colors.brown[300]),
+                                      )
+                                    ],
+                                  ),
+                                )),
+                            Expanded(
+                                flex: 2,
+                                child: Container(
+                                  color: Colors.green,
+                                ))
+                          ],
+                        ),
                       )),
                   Expanded(
                     flex: 1,
@@ -100,8 +150,14 @@ class _MainPageState extends State<MainPage> {
                             Container(
                               width: 160,
                               height: 58,
-                              decoration:
-                                  BoxDecoration(color: Colors.brown[300]),
+                              decoration: BoxDecoration(
+                                  color: Colors.brown[300],
+                                  boxShadow: const [
+                                    BoxShadow(
+                                        color: Colors.brown,
+                                        blurRadius: 2,
+                                        spreadRadius: 3)
+                                  ]),
                               child: const Center(
                                 child: Text(
                                   'LOGIN',
@@ -133,6 +189,84 @@ class _MainPageState extends State<MainPage> {
                         ),
                       ))
                 ],
+              )),
+          Positioned(
+              left: 0,
+              bottom: 0,
+              right: MediaQuery.of(context).size.width / 3 - 64,
+              child: Container(
+                height: 120,
+                decoration: const BoxDecoration(color: Colors.black),
+                padding: const EdgeInsets.only(right: 64),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Spacer(),
+                    const Text(
+                      'Join Us',
+                      style: TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(
+                      width: 64,
+                    ),
+                    Text(
+                      'Discover',
+                      style: TextStyle(
+                          color: Colors.brown[200],
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20),
+                    ),
+                    const SizedBox(
+                      width: 64,
+                    ),
+                    const Text(
+                      'Explore',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20),
+                    ),
+                    const SizedBox(
+                      width: 64,
+                    ),
+                    const Text(
+                      'Watch',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20),
+                    )
+                  ],
+                ),
+              )),
+          Positioned(
+              left: 0,
+              bottom: 0,
+              child: Container(
+                height: MediaQuery.of(context).size.height / 4,
+                width: MediaQuery.of(context).size.width / 3.5,
+                decoration: const BoxDecoration(color: Colors.grey),
+                child: Center(
+                  child: Row(
+                    children: const [
+                      Text(
+                        'WATCH NOW!',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 24),
+                      ),
+                      SizedBox(
+                        width: 8,
+                      ),
+                      Icon(
+                        Icons.play_circle_outline,
+                        color: Colors.white,
+                      )
+                    ],
+                  ),
+                ),
               ))
         ],
       ),
